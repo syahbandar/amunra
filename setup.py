16063204f2622c62f55a6c315662e688fbc7a7a8
Yuka Langbuana <yuka@wless-user-172019215201.temp.wsu.edu>
2019-03-04 16:05:05 -0800

    version 2 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
   name = 'amunra',
-  version = '0.3.0',
+  version = '0.2',
   description = 'A news portal scraping tool',
   long_description=long_description,
   long_description_content_type="text/markdown",
